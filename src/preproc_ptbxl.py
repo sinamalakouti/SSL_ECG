ad01775a7feaa33d3b3270f513af71ae1e993690
sina malakouti <siinamalakouti@gmail.com>
2022-04-22 15:20:57 -0400

    update

---
+++ src/preproc_ptbxl.py
@@ -0,0 +1,16 @@
+from clinical_ts.timeseries_utils import *
+from clinical_ts.ecg_utils import *
+from pathlib import Path
+
+target_fs=100
+#data_root=P./ecg_data/")
+target_root= Path("../ecg_data_processed")
+
+
+
+data_folder_ptb_xl = Path("../ptb_xl/ptb-xl2")
+target_folder_ptb_xl = Path("../ecg_data_processed/ptb_xl_fs100")
+df_ptb_xl, lbl_itos_ptb_xl,  mean_ptb_xl, std_ptb_xl = prepare_data_ptb_xl(data_folder_ptb_xl, min_cnt=0,  target_folder=target_folder_ptb_xl)
+
+print("here12")
+reformat_as_memmap(df_ptb_xl, target_folder_ptb_xl/("memmap.npy"),data_folder=target_folder_ptb_xl,delete_npys=True)
